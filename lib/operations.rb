05184ffba6e97236d3d09a13e000b0aa5c79e29b
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-15 21:37:09 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -7,7 +7,7 @@ end
 
 
 def not_safe?(speed)
-  speed < 40 || speed > 60 ? "Going 30 mph on the freeway would be unsafe, as would going 95 mph."
+  speed < 40 || speed > 60 ? "Going 30 mph on the freeway would be unsafe, as would going 95 mph." : "this is a safe speed"
 	
 end
 	
