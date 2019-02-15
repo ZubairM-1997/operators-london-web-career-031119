34a3400e70b5154c2ec5abb95323b3357240143f
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-15 21:38:37 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,15 @@
 def unsafe?(speed)
+  if speed < 40 || speed > 60
+    puts "Going 30 mph on the freeway would be unsafe, as would going 95 mph."
+  else
+    "this is a safe speed"
 
 end
 
 
 
 def not_safe?(speed)
+  speed < 40 || speed > 60 ? "Going 30 mph on the freeway would be unsafe, as would going 95 mph." : "this is a safe speed"
 	
 end
 	
